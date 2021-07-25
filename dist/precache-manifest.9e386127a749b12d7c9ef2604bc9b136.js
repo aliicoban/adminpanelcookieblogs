4b0282fa85b03d24f3e9018cc6d4054f94810ab5
alicoban0144 <56318755+alicoban0144@users.noreply.github.com>
2021-07-25 18:27:01 +0300

    codes transformed

---
+++ dist/precache-manifest.9e386127a749b12d7c9ef2604bc9b136.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee62b4b4953db162f19",
+    "url": "/css/app.379f225a.css"
+  },
+  {
+    "revision": "b20f3ef29d28b55dc11a",
+    "url": "/css/chunk-vendors.6f9a3ef9.css"
+  },
+  {
+    "revision": "97f2ed8d2ff7083177d1",
+    "url": "/css/widgets.fbc969bf.css"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/fonts/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/fonts/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/fonts/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/fonts/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "9e36fda288efc89d5cc8f0622fc4b34b",
+    "url": "/img/laravel-vue.svg"
+  },
+  {
+    "revision": "1eb7fff2a469da3cc6c3311572d7696d",
+    "url": "/img/placeholder.jpg"
+  },
+  {
+    "revision": "06351f128ec32c93e984c1e4d003872f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dee62b4b4953db162f19",
+    "url": "/js/app.ea6b6cf9.js"
+  },
+  {
+    "revision": "611061e7fc9123875238",
+    "url": "/js/chunk-2d21085a.104104ad.js"
+  },
+  {
+    "revision": "399c2970263bf0673462",
+    "url": "/js/chunk-469749dc.3fbe88f7.js"
+  },
+  {
+    "revision": "b20f3ef29d28b55dc11a",
+    "url": "/js/chunk-vendors.060945c7.js"
+  },
+  {
+    "revision": "4ee085b282be60f6b022",
+    "url": "/js/maps.f776e6e2.js"
+  },
+  {
+    "revision": "97f2ed8d2ff7083177d1",
+    "url": "/js/widgets.72a214e6.js"
+  },
+  {
+    "revision": "c1b8604767bdd9571f551891aaff5ba7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/.gitkeep"
+  },
+  {
+    "revision": "82afbfaa2a64cebfd0077f73abe15659",
+    "url": "/static/img/apple-icon.png"
+  },
+  {
+    "revision": "70bb8e710dd3810cd1b3fd2db9a341b4",
+    "url": "/static/img/background-2.jpg"
+  },
+  {
+    "revision": "0bd5045ff33a77784b2111a042835413",
+    "url": "/static/img/background.jpg"
+  },
+  {
+    "revision": "70bb8e710dd3810cd1b3fd2db9a341b4",
+    "url": "/static/img/background/background-2.jpg"
+  },
+  {
+    "revision": "cc3c660bb38c2221922e1407f5e66749",
+    "url": "/static/img/background/bruno-abatti.jpg"
+  },
+  {
+    "revision": "177c6847df61d8154eab67866bee0578",
+    "url": "/static/img/background/damir-bosnjak.jpg"
+  },
+  {
+    "revision": "dea64bb0006412d974553b1c308950a6",
+    "url": "/static/img/background/daniel-olahs.jpg"
+  },
+  {
+    "revision": "b8fc27de093f5dcbb7f5e43c424e698b",
+    "url": "/static/img/background/david-marcu.jpg"
+  },
+  {
+    "revision": "034ae6ed7603d4683815b56a28e6ec5e",
+    "url": "/static/img/background/jan-sendereks.jpg"
+  },
+  {
+    "revision": "5aa58f7ae76fdb3303637631fe914684",
+    "url": "/static/img/default-avatar.png"
+  },
+  {
+    "revision": "765b356c011aeb9a878e51ab2ea46c72",
+    "url": "/static/img/faces/ayo-ogunseinde-2.jpg"
+  },
+  {
+    "revision": "59cb6b8e3aae469178831eb64fccf945",
+    "url": "/static/img/faces/erik-lucatero-2.jpg"
+  },
+  {
+    "revision": "5683d329652eab2d78e159e4ab11c1b4",
+    "url": "/static/img/faces/face-0.jpg"
+  },
+  {
+    "revision": "bc90d68acaa20cbbd2ce9aaff3faa448",
+    "url": "/static/img/faces/face-1.jpg"
+  },
+  {
+    "revision": "de80389c9ee6920815e66e5a081e864e",
+    "url": "/static/img/faces/face-2.jpg"
+  },
+  {
+    "revision": "7b641618b53610f7f6691f74458a5ba4",
+    "url": "/static/img/faces/face-3.jpg"
+  },
+  {
+    "revision": "5cf2e77e952cedbacd4682f74891816a",
+    "url": "/static/img/faces/joe-gardner-1.jpg"
+  },
+  {
+    "revision": "4543601fe9f02a83a6ed48e805a767d1",
+    "url": "/static/img/faces/joe-gardner-2.jpg"
+  },
+  {
+    "revision": "8f130d5830c5c2d35f75319873c6f057",
+    "url": "/static/img/faces/kaci-baum-2.jpg"
+  },
+  {
+    "revision": "7b8a6099ce9bdadcf1bcfde26a139275",
+    "url": "/static/img/favicon.png"
+  },
+  {
+    "revision": "034ae6ed7603d4683815b56a28e6ec5e",
+    "url": "/static/img/jan-sendereks.jpg"
+  },
+  {
+    "revision": "4ec805b26cd469c628ed056227ff3df1",
+    "url": "/static/img/mike.jpg"
+  },
+  {
+    "revision": "1eb7fff2a469da3cc6c3311572d7696d",
+    "url": "/static/img/placeholder.jpg"
+  },
+  {
+    "revision": "c411a67eb76ab9a7ad26ca5d19b61392",
+    "url": "/static/img/tables/agenda.png"
+  },
+  {
+    "revision": "8e04f24a8013fe52d0851ce14713ab2a",
+    "url": "/static/img/tables/evernote.png"
+  },
+  {
+    "revision": "8d8721fccbbf6354d182a43bb1270067",
+    "url": "/static/img/tables/stylus.jpg"
+  },
+  {
+    "revision": "5285ef39fe0dd53e9cb6abba8f65409b",
+    "url": "/static/img/vue-logo.png"
+  }
+]);
